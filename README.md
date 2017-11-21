c3228657fbb823d2a925164dbea41c8faef6e0cc
Noah <nwgreen@umich.edu>
2017-11-21 12:40:45 -0500

    Simplified helix_class.py

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # HELIX Magnet Code
 ## Files:
 * /code/python/helix_class.py contains class that can read magnet data from spreadsheets in /data and calculate magnetic field from HELIX magnet
-* /code/python/run.py has examples of how to helix_class.py
+* /code/python/run.py has examples of how to use helix_class.py
--- code/python/helix_class.py
+++ code/python/helix_class.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
-import helix_mag as hlx
+import helix_mag as hlx # This is the fortran library
 
 class helix():
     def __init__(self,gdat,cldat,scldat):
@@ -17,11 +17,16 @@ class helix():
          -Note: see ../../data for spreadsheet format
         """
 
+        # Filenames
+        self.gdat = gdat
+        self.cldat = cldat
+        self.scldat = scldat
+
         # Read in general data spreadsheet
         self.__general_data = pd.read_csv(gdat)
         # Get general data column names
         g_names = list(self.__general_data)
-        self.current = self.__general_data[g_names[1]][0]
+        self.current = self.__general_data[g_names[1]][0] 
         self.contraction = [self.__general_data[g_names[2]][0],self.__general_data[g_names[3]][0]]
 
         # Read in coil data spreadsheet
@@ -76,12 +81,16 @@ class helix():
 
         # Send coil data to fortran code        
         hlx.helix_data(self.current,self.contraction,self.width,self.turns,self.ri,self.ro,self.nrho,self.nz,self.clposition,self.theta,self.phi)
+
     def print_general(self):
         print(self.__general_data)
+
     def print_coil(self):
         print(self.__coil_data)
+
     def print_subcoil(self):
         print(self.__subcoil_data)
+
     def set_coil_position(self,coil,position):
         """
         Changes position of coil.
@@ -124,7 +133,41 @@ class helix():
         return hlx.helix_magnet(x)
 #    def __check_data(self):
 #    def __set_data(self):
-    
+    def WriteData():
+        """
+        Writes current dataframes to file
+        """
+        # Make general dataframe
+        general_names = 'Current CuComp AlComp'
+        general_attr = [93.0,0.99674,0.99585]
+        general_df = pd.DataFrame([general_attr],columns=general_names.split())
+        general_df.to_csv('GeneralData.csv')
+
+        # Make dataframe for coils
+        coil_names = 'A B'
+        coil_attr = 'Width X Y Z Theta Phi'
+        coil_a = [.075818,0.,0.,-.359747,0.,0.]
+        coil_b = [.075818,0.,0.,.359747,0.,0.]
+        coil_df = pd.DataFrame([coil_a,coil_b],index=coil_names.split(),columns=coil_attr.split())
+        coil_df.index.name = 'Coil'
+        coil_df.to_csv('CoilData.csv')
+
+        # Make dataframe for subcoils
+        coil_hier1 = 'A A A B B B'.split()
+        coil_hier2 = [1,2,3,1,2,3]
+        subcoil_index = list(zip(coil_hier1,coil_hier2))
+        subcoil_index = pd.MultiIndex.from_tuples(subcoil_index)
+        subcoil_columns = ["Inner Radius","Outer Radius","Coil Turns","Nrho","Nz"]
+        sc_a1 = [.203517,.215011,1995.9,8,52]
+        sc_a2 = [.215011,.234632,5150.,8,32]
+        sc_a3 = [.234632,.255224,5489.5,8,32]
+        sc_b1 = [.203492,.214731,1976.,8,52]
+        sc_b2 = [.214732,.234709,5110.,8,32]
+        sc_b3 = [.234709,.256222,5486.7,8,32]
+        subcoil_df = pd.DataFrame([sc_a1,sc_a2,sc_a3,sc_b1,sc_b2,sc_b3],index=subcoil_index,columns=subcoil_columns)
+        subcoil_df.index.names = ['Coil','Subcoil']
+        subcoil_df.to_csv('SubcoilData.csv')
+
         
 
 
