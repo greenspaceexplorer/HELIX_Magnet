b81e17ce1e41500dc5f81c2c5560be20508cf18f
Noah <greenspaceexplorer@gmail.com>
2018-06-20 12:37:38 -0400

    Fortran single coil test working

--- src/test.cpp
+++ src/test.cpp
@@ -6,10 +6,23 @@ using namespace std;
 
 int main(){
     
+//  Single coil test
     double current = 93.0;
-    double radius = 0.01;
-    Vector3d 
-    // test ideal coil
+    double radius = 0.255224;
+    Vector3d b_field;
+    // first test on axis
+    Vector3d on_axis(0.0,0.0,0.1);
+    b_field = b_loop(current,radius,on_axis);
+    cout << 
+        "(X Y Z) = (" <<
+        on_axis(0) << " " << 
+        on_axis(1) << " " <<
+        on_axis(2) << ")" << endl;
+    cout <<
+        "(BX BY BZ) = (" <<
+        b_field(0) << " " <<
+        b_field(1) << " " <<
+        b_field(2) << ")" << endl;
     
 
     return 0;
